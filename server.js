336de91aca8476f5418368ae30bf94a6af3f773a
Swaraj <swarajchavan2004@gmail.com>
2023-08-05 16:11:52 +0530

    fixed paths to html

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 let path = require("path");
 
-app.listen(3000);
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/home.html"));
+  res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 app.get("/it-services", (req, res) => {
-  res.sendFile(path.join(__dirname + "/itservices.html"));
+  res.sendFile(path.join(__dirname + "/views/itservices.html"));
 });
 
 app.get("/about-us", (req, res) => {
-  res.sendFile(path.join(__dirname + "/aboutus.html"));
+  res.sendFile(path.join(__dirname + "/views/aboutus.html"));
 });
+
+app.listen(3000);
